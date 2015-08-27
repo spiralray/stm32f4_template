37ce3426d57c5cffb0989f061278c400297e33f6
Kentaro Tanaka <spiralray@rakusei.net>
2015-08-27 13:48:40 +0900

    Declare CPU_CLOCK_HZ using SystemCoreClock

--- inc/FreeRTOSConfig.h
+++ inc/FreeRTOSConfig.h
@@ -82,12 +82,14 @@
 #ifdef __ICCARM__
 	#include <stdint.h>
 	extern uint32_t SystemCoreClock;
+#else
+	#include "system_stm32f4xx.h"
 #endif
 
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( 168000000 )
+#define configCPU_CLOCK_HZ				( SystemCoreClock )
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 1024 )
