d1d607f53422939b18229e269838c0ddbde54fe7
Kentaro Tanaka <spiralray@rakusei.net>
2015-11-13 23:11:04 +0900

    Fix sample program

--- src/sample/f4discovery/adc_single_dma_multichan/main.cpp
+++ src/sample/f4discovery/adc_single_dma_multichan/main.cpp
@@ -12,142 +12,143 @@
 
 #include "utils/debug.h"
 
-
 using namespace stm32plus;
 
-//DMA buffer must be static to avoid assigning to CCM
-volatile uint16_t buffer[4];
+
+/**
+ * This example adds DMA and multi-channel conversion to the mix. We'll use ADC1 to convert
+ * three channels plus the internal temperature automatically and in sequence and we'll
+ * write out the results to the USART for you to see.
+ * On the F4:
+ *   The ADC is configured in 'scan mode' which means that it will convert all the
+ *   configured channels and, because we are not using continuous mode, it will stop at the end
+ *   of the group.
+ *
+ * The DMA channel for ADC1 is used to move the converted channel data out to SRAM.
+ * We configure the 'complete' DMA interrupt to fire when the complete group has
+ * finished converting. The converted data is written to the USART, we pause for a second
+ * and then do it all again, ad infinitum.
+ *
+ * USART1 is configured with protocol settings of 57600/8/N/1. The ADC channels are read
+ * from PA[0], PA[1]. You will need to connect these GPIO inputs to valid levels
+ * between GND and VREF to see conversion values.
+ */
 
 
 class AdcSingleDmaMultiChan {
 
-private:
-	volatile bool _ready;
-	uint16_t *readBuffer;
+  private:
+    volatile bool _ready;
 
-public:
+  public:
 
-	AdcSingleDmaMultiChan(uint16_t *_buffer){
-		readBuffer = _buffer;
-	}
+    void run() {
 
-	void run() {
+      _ready=false;
 
-		_ready=false;
+      /*
+       * We're converting 2 channels in circular buffer mode so we need exactly 4 16-bit words
+       * for that circular buffer
+       */
 
-		/*
-		 * Declare the ADC1 DMA channel. The default is circular mode for the AdcDmaFeature
-		 * which means that it wil automatically refill our buffer on each conversion because
-		 * one conversion exactly matches the size of the memory buffer that we will give
-		 * to the DMA peripheral.
-		 */
+      volatile uint16_t readBuffer[2];
 
-		Adc1DmaChannel<AdcDmaFeature<Adc1PeripheralTraits>,Adc1DmaChannelInterruptFeature> dma;
+      /*
+       * Declare the ADC1 DMA channel. The default is circular mode for the AdcDmaFeature
+       * which means that it wil automatically refill our buffer on each conversion because
+       * one conversion exactly matches the size of the memory buffer that we will give
+       * to the DMA peripheral.
+       */
 
-		/*
-		 * Declare the ADC peripheral with an APB2 clock prescaler of 2, a resolution of
-		 * 12 bits. We will use 144-cycle conversions on ADC channels 0,1 and a 480-cycle
-		 * conversion on ADC channel 2. Scan mode is used with the default template parameter
-		 * that causes EOC to be raised at the end of a complete conversion group.
-		 */
+      Adc1DmaChannel<AdcDmaFeature<Adc1PeripheralTraits>,Adc1DmaChannelInterruptFeature> dma;
 
-		Adc1<
-		AdcClockPrescalerFeature<2>,                // prescaler of 2
-		AdcResolutionFeature<12>,                   // 12 bit resolution
-		Adc1Cycle144RegularChannelFeature<0,1>,     // using channels 0,1 on ADC1 with 144-cycle latency
-		Adc1Cycle480RegularChannelFeature<2>,       // using channel 2 on ADC1 with 480-cycle latency
-		Adc1Cycle480TemperatureSensorFeature,       // using the temperature sensor channel
-		AdcScanModeFeature<>                        // scan mode with EOC after each group
-		> adc;
+      /*
+       * Declare the ADC peripheral with an APB2 clock prescaler of 2, a resolution of
+       * 12 bits. We will use 144-cycle conversions on ADC channels 0,1
+       * that causes EOC to be raised at the end of a complete conversion group.
+       */
 
-		/*
-		 * Subscribe to the DMA complete interrupt
-		 */
+      Adc1<
+        AdcClockPrescalerFeature<2>,                // prescaler of 2
+        AdcResolutionFeature<12>,                   // 12 bit resolution
+        Adc1Cycle144RegularChannelFeature<0,1>,     // using channels 0,1 on ADC1 with 144-cycle latency
+        AdcScanModeFeature<>                        // scan mode with EOC after each group
+      > adc;
 
-		dma.DmaInterruptEventSender.insertSubscriber(
-				DmaInterruptEventSourceSlot::bind(this,&AdcSingleDmaMultiChan::onComplete)
-		);
+      /*
+       * Subscribe to the DMA complete interrupt
+       */
 
-		/*
-		 * Enable the DMA interrupt
-		 */
+      dma.DmaInterruptEventSender.insertSubscriber(
+          DmaInterruptEventSourceSlot::bind(this,&AdcSingleDmaMultiChan::onComplete)
+      );
 
-		dma.enableInterrupts(Adc1DmaChannelInterruptFeature::COMPLETE);
+      /*
+       * Enable the DMA interrupt
+       */
 
-		printf("Data buffer address is 0x%08x\r\n", (uint32_t)readBuffer);
+      dma.enableInterrupts(Adc1DmaChannelInterruptFeature::COMPLETE);
 
-		if( (uint32_t)readBuffer >= 0x10000000 && (uint32_t)readBuffer < 0x10000000 + 64*1024 ){
-			debug << "ERROR! Data buffer is on Core-Coupled SRAM! DMA cannot be used on CCM.\r\n";
-		}
+      /**
+       * start the DMA (i.e. make it ready to receive requests from the ADC peripheral)
+       */
 
-		/**
-		 * start the DMA (i.e. make it ready to receive requests from the ADC peripheral)
-		 */
+      dma.beginRead(readBuffer,2);
 
-		dma.beginRead(readBuffer,4);
+      /*
+       * Go into an infinite loop converting
+       */
 
-		/*
-		 * Go into an infinite loop converting
-		 */
+      for(;;) {
 
-		for(;;) {
+        /*
+         * For this test code we set the 4 words to a known pattern so we can tell if they
+         * get properly overwritten by the DMA during conversion
+         */
+        readBuffer[0]=readBuffer[1]=0xAAAA;
 
-			/*
-			 * For this test code we set the 4 words to a known pattern so we can tell if they
-			 * get properly overwritten by the DMA during conversion
-			 */
+        /*
+         * Start a conversion and wait until the interrupt handler tells us
+         * that it's finished.
+         */
+        adc.startRegularConversion();
 
-			readBuffer[0]=readBuffer[1]=readBuffer[2]=readBuffer[3]=0xAAAA;
+        while(!_ready);
+        _ready=false;
+#ifdef DEBUG_PORT
+        /*
+         * write the values to the USART
+         */
+        debug << "Converted values are "
+                     << StringUtil::Ascii(readBuffer[0]) << ", "
+                     << StringUtil::Ascii(readBuffer[1]) << "\r\n";
+#endif
 
-			/*
-			 * Start a conversion and wait until the interrupt handler tells us
-			 * that it's finished.
-			 */
-
-			adc.startRegularConversion();
-
-			while(!_ready);
-			_ready=false;
-
-			/*
-			 * write the values to the USART
-			 */
-
-			debug << "Converted values are "
-					<< StringUtil::Ascii(readBuffer[0]) << ", "
-					<< StringUtil::Ascii(readBuffer[1]) << ", "
-					<< StringUtil::Ascii(readBuffer[2]) << ", temperature="
-					<< StringUtil::Ascii(adc.getTemperature(readBuffer[3])) << "\r\n";
-
-			// wait for a second before converting the next set of values
-
-			MillisecondTimer::delay(1000);
-		}
-	}
+        // wait for a second before converting the next set of values
+        MillisecondTimer::delay(100);
+      }
+    }
 
 
-	/**
-	 * Interrupt handler for the DMA complete event. Set the ready flag
-	 * when it's received.
-	 */
+    /**
+     * Interrupt handler for the DMA complete event. Set the ready flag
+     * when it's received.
+     */
 
-	void onComplete(DmaEventType det) {
-		if(det==DmaEventType::EVENT_COMPLETE)
-			_ready=true;
-	}
+    void onComplete(DmaEventType det) {
+      if(det==DmaEventType::EVENT_COMPLETE)
+        _ready=true;
+    }
 };
 
 int main() {
 
-	// we're using interrupts, initialise NVIC
+  Nvic::initialise();
+  MillisecondTimer::initialise();
 
-	Nvic::initialise();
+  AdcSingleDmaMultiChan adc;
+  adc.run();
 
-	MillisecondTimer::initialise();
-
-	AdcSingleDmaMultiChan adc(buffer);
-	adc.run();
-
-	// not reached
-	return 0;
+  // not reached
+  return 0;
 }
