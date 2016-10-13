f2574189aa7207a1aa3b5198bd78410876282a69
kenkentan <ken.kentan.k2@gmail.com>
2016-10-13 14:26:36 +0900

    fix rosserial

--- lib/ros_lib/STM32Hardware.h
+++ lib/ros_lib/STM32Hardware.h
@@ -1,21 +1,22 @@
 #pragma once
 
 #include "config/stm32plus.h"
-#include "utils/UsartWithBuffer.h"
+#include "utils/UsartInterruptWithBuffer.h"
 
 class STM32Hardware {
 
-  using SERIAL_CLASS = UsartWithBuffer<Usart2_Remap1<Usart2InterruptFeature>, Usart2InterruptFeature>;
+  //MainV3 FT232 debug port
+  using SERIAL_CLASS = UsartInterruptWithBuffer<Usart2_Remap1<Usart2InterruptFeature>>;
 
   public:
-  STM32Hardware(SERIAL_CLASS* _com , long baud= 460800){
+  STM32Hardware(SERIAL_CLASS* _com , long baud= 115200){//default 460800
       com = _com;
       baud_ = baud;
     }
     STM32Hardware()
     {
-      com = new SERIAL_CLASS(460800);
-      baud_ = 460800;
+      com = new SERIAL_CLASS(115200);
+      baud_ = 115200;
     }
     STM32Hardware(STM32Hardware& h){
       this->baud_ = h.baud_;
@@ -33,7 +34,7 @@ class STM32Hardware {
 
     int read(){
       if(com->dataAvailable()){
-	  return com->getch();
+	  return com->receive();
       }
       else{
 	  return -1;
@@ -42,8 +43,8 @@ class STM32Hardware {
 
     void write(uint8_t* data, int length){
       for(int i=0; i<length; i++){
-	while( !com->TXBuffer_FreeSpace() );
-	com->putch(data[i]);
+	while( !com->readyToSend() );
+	com->send(data[i]);
       }
 
     }
